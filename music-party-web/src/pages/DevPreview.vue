6d3b42fb49b7568206e6b4ecb17bf1b8804e871b
Nirotiy <1187386764@qq.com>
2026-05-15 13:13:31 +0800

    feat: implement queue auto-suppression, layout optimizations and add design/product docs

---
+++ music-party-web/src/pages/DevPreview.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="h-screen w-screen bg-[#0B0B0C] flex items-center justify-center">
+    <SearchRedesign />
+  </div>
+</template>
+
+<script setup>
+import SearchRedesign from '../components/SearchRedesign.vue';
+</script>
